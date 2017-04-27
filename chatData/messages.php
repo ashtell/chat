1e3cbed4985d5c0ae5215226d61207a1637c3d0b
root <root@localhost.localdomain>
2017-04-26 18:25:23 -0600

    first commit

---
+++ chatData/messages.php
@@ -0,0 +1,43 @@
+<?php echo ("04/26/2017 : 12:21 PM &nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; !-!-!-!-!- SYSTEM:: Mikey Logged out. -!-!-!-!-!"); ?><br>
+<?php echo ("04/26/2017 : 12:21 PM &nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ---------- SYSTEM:: Mikey Logged in. ----------"); ?><br>
+<?php echo ("04/26/2017 : 12:22 PM &nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; !-!-!-!-!- SYSTEM:: Mikey Logged out. -!-!-!-!-!"); ?><br>
+<?php echo ("04/26/2017 : 12:22 PM &nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ---------- SYSTEM:: Mikey Logged in. ----------"); ?><br>
+<?php echo ("04/26/2017 : 12:23 PM &nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; !-!-!-!-!- SYSTEM:: Mikey Logged out. -!-!-!-!-!"); ?><br>
+<?php echo ("04/26/2017 : 12:23 PM &nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ---------- SYSTEM:: Mikey Logged in. ----------"); ?><br>
+<?php echo ("04/26/2017 : 12:23 PM &nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; !-!-!-!-!- SYSTEM:: Mikey Logged out. -!-!-!-!-!"); ?><br>
+<?php echo ("04/26/2017 : 12:23 PM &nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ---------- SYSTEM:: Mikey Logged in. ----------"); ?><br>
+<?php echo ("04/26/2017 : 12:26 PM &nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; !-!-!-!-!- SYSTEM:: Mikey Logged out. -!-!-!-!-!"); ?><br>
+<?php echo ("04/26/2017 : 12:26 PM &nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ---------- SYSTEM:: Mikey Logged in. ----------"); ?><br>
+<?php echo ("04/26/2017 : 12:27 PM &nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; !-!-!-!-!- SYSTEM:: Mikey Logged out. -!-!-!-!-!"); ?><br>
+<?php echo ("04/26/2017 : 12:27 PM &nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ---------- SYSTEM:: Mikey Logged in. ----------"); ?><br>
+<?php echo ("04/26/2017 : 12:27 PM &nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  Mikey: Hello"); ?><br>
+<?php echo ("04/26/2017 : 12:27 PM &nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  Mikey: no"); ?><br>
+<?php echo ("04/26/2017 : 12:27 PM &nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  Mikey: just"); ?><br>
+<?php echo ("04/26/2017 : 12:27 PM &nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  Mikey: work"); ?><br>
+<?php echo ("04/26/2017 : 12:27 PM &nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  Mikey: hwo"); ?><br>
+<?php echo ("04/26/2017 : 12:27 PM &nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  Mikey: long"); ?><br>
+<?php echo ("04/26/2017 : 12:27 PM &nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  Mikey: is"); ?><br>
+<?php echo ("04/26/2017 : 12:27 PM &nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  Mikey: this"); ?><br>
+<?php echo ("04/26/2017 : 12:27 PM &nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  Mikey: list"); ?><br>
+<?php echo ("04/26/2017 : 12:28 PM &nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; !-!-!-!-!- SYSTEM:: Mikey Logged out. -!-!-!-!-!"); ?><br>
+<?php echo ("04/26/2017 : 12:28 PM &nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ---------- SYSTEM:: Mikey Logged in. ----------"); ?><br>
+<?php echo ("04/26/2017 : 12:31 PM &nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  Mikey: How"); ?><br>
+<?php echo ("04/26/2017 : 12:31 PM &nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  Mikey: No"); ?><br>
+<?php echo ("04/26/2017 : 12:33 PM &nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  Mikey: No"); ?><br>
+<?php echo ("04/26/2017 : 12:33 PM &nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; !-!-!-!-!- SYSTEM:: Mikey Logged out. -!-!-!-!-!"); ?><br>
+<?php echo ("04/26/2017 : 12:33 PM &nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ---------- SYSTEM:: Mikey Logged in. ----------"); ?><br>
+<?php echo ("04/26/2017 : 02:38 PM &nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  : Ugh"); ?><br>
+<?php echo ("04/26/2017 : 02:38 PM &nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ---------- SYSTEM:: Mikey Logged in. ----------"); ?><br>
+<?php echo ("04/26/2017 : 02:40 PM &nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ---------- SYSTEM:: Mikey Logged in. ----------"); ?><br>
+<?php echo ("04/26/2017 : 02:40 PM &nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  Mikey: Ugh"); ?><br>
+<?php echo ("04/26/2017 : 02:42 PM &nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  Mikey: Ugh"); ?><br>
+<?php echo ("04/26/2017 : 02:42 PM &nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  Mikey: Ugh"); ?><br>
+<?php echo ("04/26/2017 : 02:46 PM &nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  Mikey: Ugh"); ?><br>
+<?php echo ("04/26/2017 : 02:48 PM &nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  Mikey: Ugh"); ?><br>
+<?php echo ("04/26/2017 : 02:49 PM &nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  Mikey: Hello"); ?><br>
+<?php echo ("04/26/2017 : 05:49 PM &nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; !-!-!-!-!- SYSTEM::  Logged out. -!-!-!-!-!"); ?><br>
+<?php echo ("04/26/2017 : 05:50 PM &nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ---------- SYSTEM:: Mikey Logged in. ----------"); ?><br>
+<?php echo ("04/26/2017 : 05:53 PM &nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  Mikey: Hello, Mikey Says Hi"); ?><br>
+<?php echo ("04/26/2017 : 05:56 PM &nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; !-!-!-!-!- SYSTEM:: Mikey Logged out. -!-!-!-!-!"); ?><br>
+<?php echo ("04/26/2017 : 05:56 PM &nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ---------- SYSTEM:: Test Logged in. ----------"); ?><br>
+<?php echo ("04/26/2017 : 05:57 PM &nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; !-!-!-!-!- SYSTEM:: Test Logged out. -!-!-!-!-!"); ?><br>
